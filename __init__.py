c98d5426a8c9f8e155649c200c1dd9cd2ec9d99f
M. Alonso <port02.server@gmail.com>
2017-01-31 12:39:58 +0000

    initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#This file is part party_vat_unique module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
+from trytond.pool import Pool
+from .party import *
+
+def register():
+    Pool.register(
+        Party,
+        module='party_name_unique', type_='model')
---
+++ party.py
@@ -0,0 +1,50 @@
+#This file is part party_name_unique module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
+
+
+from trytond.pool import Pool, PoolMeta
+from trytond.transaction import Transaction
+
+
+__all__ = ['Party']
+
+
+class Party:
+    __metaclass__ = PoolMeta
+    __name__ = 'party.party'
+
+    @classmethod
+    def copy(cls, parties, default=None):
+        if default is None:
+            default = {}
+        default['name'] = None
+        return super(Party, cls).copy(parties, default=default)
+
+    @classmethod
+    def validate(cls, parties):
+        super(Party, cls).validate(parties)
+        for party in parties:
+            party.strip_name()
+            party.unique_name()
+
+    def strip_name(self):
+        #Warn on existing name and constraint non stripped names
+        if (self.id > 0):
+            if not (self.name == self.name.strip()):
+                self.raise_user_error(
+                    "Party name should be stripped!")
+
+    def unique_name(self):
+        user = Transaction().user
+
+        if (self.id > 0 and user > 1):
+            parties = Pool().get('party.party')
+            #Must explicitly search on records with active=False
+            #otherwise only search on records with active=True
+            parties_count = parties.search_count(
+                [('name', '=', self.name), ('active', 'in', (True, False))])
+            if (parties_count > 1):
+                self.raise_user_warning('warn_party_with_same_name.%d' % self.id,
+                    'Party name "%s" already exists!', self.rec_name)
+
