4d6cbc9aeaafecaa507c8d7eac2c12566ba54961
M. Alonso <port02.server@gmail.com>
2018-03-19 11:24:45 +0000

    update version

--- party.py
+++ party.py
@@ -1,5 +1,5 @@
 #This file is part party_name_unique module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 
 
