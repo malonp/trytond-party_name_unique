9eafff8b17437fc7538724a358f5fa4c929390a7
map <port02.server@gmail.com>
2019-05-12 21:55:23 +0100

    update version

--- __init__.py
+++ __init__.py
@@ -1,10 +1,9 @@
-#This file is part party_vat_unique module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part party_vat_unique module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.pool import Pool
 from .party import *
 
+
 def register():
-    Pool.register(
-        Party,
-        module='party_name_unique', type_='model')
+    Pool.register(Party, module='party_name_unique', type_='model')
